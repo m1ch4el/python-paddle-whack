f7ee15b1c20c7c14f462742a6946e8316e1673d3
ttreitlinger <ttreitlinger@gmail.com>
2013-07-12 23:09:09 +0100

    initial commit

---
+++ paddlewhack.py
@@ -0,0 +1,17 @@
+from tkinter import *
+import time
+
+app = Tk()
+
+app.title("Paddle Whack")
+app.resizable(0, 0)                 # make window a fixed size
+app.wm_attributes("-topmost", 1)    # bring window to front
+
+# create a canvas 500 by 400 pixels, with no border
+canvas = Canvas(app, width=500, height=400,
+                bd=0, highlightthickness=0)
+
+canvas.pack()       # tell canvas to size itself
+app.update()        # without this the canvas height and width is not set correctly
+
+app.mainloop()      # hang around until the window is closed...
