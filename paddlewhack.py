c66782792645338cf0996e68e7f9176a0387c411
ttreitlinger <ttreitlinger@gmail.com>
2013-07-12 23:54:07 +0100

    bind keypress left/right to move paddle

--- paddlewhack.py
+++ paddlewhack.py
@@ -35,10 +35,24 @@ class Paddle:
         self.canvas = canvas
         self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
         self.canvas.move(self.id, 200, 300)
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
         
     def draw(self):
-        pass
-
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords(self.id)
+        
+        if pos[0] <= 0 or pos[2] >= self.canvas_width:
+            self.x = 0;
+            
+    def turn_left(self, event):
+        self.x = -2
+        
+    def turn_right(self, event): 
+        self.x = 2
+        
 # a helper function to reverse a direction
 def reverse(direction):
     return direction * -1
