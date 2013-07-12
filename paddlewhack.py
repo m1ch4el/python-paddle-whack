ae6ed87a26dc382ab28b7b5a5a55eb18aa47220f
ttreitlinger <ttreitlinger@gmail.com>
2013-07-13 00:29:21 +0100

    detect if ball hit the paddle, and bounce back

--- paddlewhack.py
+++ paddlewhack.py
@@ -5,8 +5,9 @@ import random
 
 # define the Ball
 class Ball:
-    def __init__(self, canvas, color):    # need to draw on a canvas, and in a given colour
+    def __init__(self, canvas, paddle, color):    # need to draw on a canvas, and in a given colour
         self.canvas = canvas
+        self.paddle = paddle        # also need to know where the paddle is
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color) # coordinates: top-l, bot-r
         self.canvas.move(self.id, 245, 100);   # move 245 px right, 100 down
         self.x = random.randint(-3, 3)      # how many pixels will we move right initially?
@@ -27,6 +28,17 @@ class Ball:
             self.x = reverse(self.x)        # ... reverse left/right direction
         if pos[2] >= self.canvas_width:     # if right side of ball hits right screen border
             self.x = reverse(self.x)        # ... reverse left/right direction
+
+        if self.hit_paddle():
+            self.y = reverse(self.y)
+            
+    def hit_paddle(self):
+        paddle_pos = self.canvas.coords(self.paddle.id)
+        ball_pos = self.canvas.coords(self.id)
+        if ball_pos[2] >= paddle_pos[0] and ball_pos[0] <= paddle_pos[2]:
+            if ball_pos[3] >= paddle_pos[1] and ball_pos[3] <= paddle_pos[3]:
+                return True
+        return False
 
 
 # define the Paddle
@@ -72,8 +84,8 @@ canvas = Canvas(app, width=500, height=400,
 canvas.pack()       # tell canvas to size itself
 app.update()        # without this the canvas height and width is not set correctly
 
-ball = Ball(canvas, 'red')      # create a red ball
 paddle = Paddle(canvas, 'blue') # create a blue paddle
+ball = Ball(canvas, paddle, 'red')      # create a red ball
 
 
 # use our own game loop
